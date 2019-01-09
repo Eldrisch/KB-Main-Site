23df0791dd44387b5a229c23cc35e41122c10409
Kacper <bara1211@gmail.com>
2019-01-09 16:50:54 +0100

    Sticky nav bar on scroll

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,16 +3,26 @@ import { Link } from "react-router-dom";
 import '../styles/Nav.css';
 
 class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+  }
   render() {
     return (
-      <nav className="navigation_bar">
-        <Link to="/">Strona główna</Link>
-        <Link to="/about/">O mnie</Link>
-        <Link to="/skills/">Moje umiejętności</Link>
-        <Link to="/projects/">Moje projekty</Link>
-        <Link to="/contact/">Kontakt</Link>
+      <nav className="navigation_bar" ref={this.myRef}>
+        <Link to="/" onClick={this.scrollToMyRef}>Strona główna</Link>
+        <Link to="/about/" onClick={this.scrollToMyRef}>O mnie</Link>
+        <Link to="/skills/" onClick={this.scrollToMyRef}>Moje umiejętności</Link>
+        <Link to="/projects/" onClick={this.scrollToMyRef}>Moje projekty</Link>
+        <Link to="/contact/" onClick={this.scrollToMyRef}>Kontakt</Link>
       </nav>
     );
+  }
+  scrollToMyRef = () => {
+    window.scrollTo({
+      top: this.myRef.current.offsetTop,
+      behavior: "smooth"
+    })
   }
 }
 
